d7bb3069087e9424237c59d0cb959abea73192e0
Luis Escobedo <escobedotrenado@gmail.com>
2022-08-23 18:21:11 -0500

    Mejorando vista

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ $(window).ready(function(){
             data = JSON.parse(data);
 
             if(data == 'true'){
-                location.href = 'misPropiedades.html';
+                location.href = 'mainAdmin.html';
             } else{
                 swal.fire({
                     title: 'Error',
