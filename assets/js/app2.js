64c5983fb98cf57ea9e4650468bf746f489585bf
GuichoMX <escobedotrenado@gmail.com>
2022-09-28 09:23:59 -0500

    Agregando Log in solo con JS

---
+++ assets/js/app2.js
@@ -0,0 +1,25 @@
+let correctEmail = 'escobedotrenado@gmail.com'
+let correctPwd = 'test123'
+
+let emailInput = document.getElementById('txt_usuario')
+let pwdInput = document.getElementById('txt_contrasena')
+
+let button = document.getElementById('btnSubmit')
+
+const logIn = function(e){
+    e.preventDefault();
+    if (correctEmail === emailInput.value && correctPwd === pwdInput.value){
+        location.href = 'mainAdmin.html';
+        button.disabled = true
+        button.removeEventListener('click', logIn)
+    } else{
+        swal.fire({
+            title: 'Error',
+            icon: 'error',
+            text: 'Usuario o contraseña incorrecta, intenta nuevamente!'
+        });
+    }
+}
+
+button.addEventListener('click', logIn)
+
